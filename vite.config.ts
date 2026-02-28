276269f47c70eae6b86c63c368c1d02c6f5a8891
BEJAOUI MOHAMED <mohamedbejaoui1253@gmail.com>
2026-02-28 18:18:02 +0000

    Trigger Netlify rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// Trigger Netlify rebuild
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
